d1c27ed3a067777fdcd2d81d687c27e95c3d8f4f
abderrahim-elouardi <elouardiabderrahim06@gmil.com>
2026-04-24 13:54:35 +0100

    initilisation de projet

---
+++ backend/scienceMedia/src/main/java/com/fsdm/wisd/scienceMedia/ScienceMediaApplication.java
@@ -0,0 +1,13 @@
+package com.fsdm.wisd.scienceMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceMediaApplication.class, args);
+	}
+
+}
---
+++ backend/scienceMedia/src/test/java/com/fsdm/wisd/scienceMedia/ScienceMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsdm.wisd.scienceMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
